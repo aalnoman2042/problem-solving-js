b78f095ac9c12d0d7c69d30580d66c3524234ff7
aalnoman <abdullahalnoman2042@gmail.com>
2023-02-04 22:50:28 +0600

    5 problem solved

--- problems.js
+++ problems.js
@@ -520,14 +520,30 @@ const myBookNumbers = paperRequirements(1, 2, 1);
 
 /*  একটা ফাংশন লিখবে। এই ফাংশনের নাম হবে bestFriend তারপর সেই ফাংশনে ইনপুট প্যারামিটার হিসেবে একটা array নিবে। সেই array এর মধ্যে তোমার সব ফ্রেন্ডের নাম থাকবে। এখন তোমার কাজ হচ্ছে যে ফ্রেন্ড এর নাম সবচেয়ে বড় সেই ফ্রেন্ড এর নাম রিটার্ন করে দেয়া। এই ক্ষেত্রে তুমি নামটা অর্থাৎ ফ্রেন্ডের নাম (স্ট্রিং) রিটার্ন করতে হবে।  */
 
-// function bestFriend(names){
-//   let convert = names.toString()
-//  var cut = convert.length();
-//  console.log(cut);
-// }
+function bestFriend(names){
+  let convert = names.toString()
+   let name = convert.split(',')
+   let bigName = [""];
+   
+  for(let i = 0; i <name.length; i++){
+    let element = name[i]
+     if( element.length > bigName.length ){
+      bigName = element;
+    }
+  }
+return bigName.toString();
 
-// const friendsName = ["rahee", "riko", "ahad", "faruk", "tamjid", "fahad", "mahin"]
-// const allNames = bestFriend(friendsName);
+}
+
+const friendsName = ["rahee", "riko", "mahin", "ahad", "tamjid", "faruk", "fahad", "adullah"]
+const allNames = bestFriend(friendsName);
+console.log(allNames);
+console.log(typeof(allNames));
+
+
+
+
+
 
 
 /* . একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে। */
@@ -547,4 +563,4 @@ function positiveNumbers(numbers) {
 }
 const allNumbers = [10, 20, -86, 60, -68, 78, -35, -92]
 const callingFunc = positiveNumbers(allNumbers)
-console.log(callingFunc);
+// console.log(callingFunc);
